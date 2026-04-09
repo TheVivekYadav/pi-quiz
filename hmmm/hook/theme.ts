8f7b136733b908f3f7574afc657decc31b78f4ea
thevivekyadav <vivekyadavclasher@gmail.com>
2026-04-09 14:15:30 +0530

    feat: simplify theme hook to always return light colors

--- hmmm/hook/theme.ts
+++ hmmm/hook/theme.ts
@@ -1,7 +1,5 @@
-import { useColorScheme } from 'react-native';
 import Colors from '../constants/Colors';
 
 export const useTheme = () => {
-  const scheme = useColorScheme(); // 'dark' or 'light'
-  return scheme === 'dark' ? Colors.dark : Colors.light;
+  return Colors.light;
 };
